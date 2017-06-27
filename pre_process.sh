64c0a030b4a13d3058377017eb47aadd39b4a6da
MoriKen254 <masamorobo@gmail.com>
2017-06-28 02:44:04 +0900

    Remove preprocess

--- pre_process.sh
+++ pre_process.sh
@@ -4,7 +4,7 @@ echo "pwd"
 pwd
 echo "ls"
 ls
-add-apt-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl
-apt-get update
-apt-get install -yV libpcl-all
+echo "yes | sudo apt-add-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl; sudo apt-get update -qq; sudo apt-get install -y libpcl-all;"
+yes | sudo apt-add-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl; sudo apt-get update -qq; sudo apt-get install -y libpcl-all;
+echo ""
 echo "pre process has been done!"
